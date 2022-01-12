e245367043730d5e11eef318e4a4fdb8372831e9
carter <zongu3317@gmail.com>
2022-01-12 14:03:25 +0800

    [added] 加入持久層單元測試專案 [added] 加入Suject持久層測試

---
+++ ReportCard.Persistent.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ReportCard.Persistent.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ReportCard.Persistent.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7ac2b4df-0be7-4347-9e71-fe341c942c1b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ReportCard.Persistent.Tests/Repository/SujectRepositoryTests.cs
@@ -0,0 +1,121 @@
+﻿
+namespace ReportCard.Persistent.Tests.Repository
+{
+using System;
+    using System.Data.SqlClient;
+    using System.Linq;
+    using Dapper;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using ReportCard.Persistent.Model;
+    using ReportCard.Persistent.Repository;
+
+    [TestClass]
+    public class SujectRepositoryTests
+    {
+        private const string connectionString = @"Data Source=localhost;database=ReportCard;Integrated Security=True";
+
+        private ISujectRepository repo;
+
+        [TestInitialize]
+        public void Init()
+        {
+            var sqlStr = @"TRUNCATE TABLE t_suject";
+
+            using (var cn = new SqlConnection(connectionString))
+            {
+                cn.Execute(sqlStr);
+            }
+
+            this.repo = new SujectRepository(connectionString);
+        }
+
+        [TestMethod]
+        public void 新增科目測試()
+        {
+            var result = this.repo.Add("TEST001");
+
+            Assert.IsNull(result.exception);
+            Assert.IsNotNull(result.suject);
+            Assert.AreEqual(result.suject.f_id, 1);
+            Assert.AreEqual(result.suject.f_name, "TEST001");
+        }
+
+        [TestMethod]
+        public void 移除科目測試()
+        {
+            var delResult = this.repo.Delete(1);
+
+            Assert.IsNull(delResult.exception);
+            Assert.IsNull(delResult.suject);
+
+            var addResult = this.repo.Add("TEST001");
+
+            Assert.IsNull(addResult.exception);
+            Assert.IsNotNull(addResult.suject);
+            Assert.AreEqual(addResult.suject.f_id, 1);
+            Assert.AreEqual(addResult.suject.f_name, "TEST001");
+
+            delResult = this.repo.Delete(1);
+
+            Assert.IsNull(delResult.exception);
+            Assert.IsNotNull(delResult.suject);
+            Assert.AreEqual(delResult.suject.f_id, 1);
+            Assert.AreEqual(delResult.suject.f_name, "TEST001");
+        }
+
+        [TestMethod]
+        public void 取得所有科目測試()
+        {
+            var addResult = this.repo.Add("TEST001");
+
+            Assert.IsNull(addResult.exception);
+            Assert.IsNotNull(addResult.suject);
+            Assert.AreEqual(addResult.suject.f_id, 1);
+            Assert.AreEqual(addResult.suject.f_name, "TEST001");
+
+            addResult = this.repo.Add("TEST002");
+
+            Assert.IsNull(addResult.exception);
+            Assert.IsNotNull(addResult.suject);
+            Assert.AreEqual(addResult.suject.f_id, 2);
+            Assert.AreEqual(addResult.suject.f_name, "TEST002");
+
+            var queryResult = this.repo.Query();
+
+            Assert.IsNull(queryResult.exception);
+            Assert.IsNotNull(queryResult.sujects);
+            Assert.AreEqual(queryResult.sujects.Count(), 2);
+        }
+
+        [TestMethod]
+        public void 更新科目測試()
+        {
+            var updateReuslt = this.repo.Update(new Suject()
+            {
+                f_id = 1,
+                f_name = "UPDATE001"
+            });
+
+            Assert.IsNull(updateReuslt.exception);
+            Assert.IsNull(updateReuslt.suject);
+
+            var addResult = this.repo.Add("TEST001");
+
+            Assert.IsNull(addResult.exception);
+            Assert.IsNotNull(addResult.suject);
+            Assert.AreEqual(addResult.suject.f_id, 1);
+            Assert.AreEqual(addResult.suject.f_name, "TEST001");
+
+            updateReuslt = this.repo.Update(new Suject()
+            {
+                f_id = 1,
+                f_name = "UPDATE001"
+            });
+
+            Assert.IsNull(updateReuslt.exception);
+            Assert.IsNotNull(updateReuslt.suject);
+            Assert.AreEqual(updateReuslt.suject.f_id, 1);
+            Assert.AreEqual(updateReuslt.suject.f_name, "UPDATE001");
+        }
+    }
+}
