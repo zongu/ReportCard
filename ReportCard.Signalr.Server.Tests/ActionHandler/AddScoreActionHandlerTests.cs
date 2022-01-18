5dec36cab58e33ecc34f25f76303b22ace04f8b3
carter <zongu3317@gmail.com>
2022-01-18 14:27:48 +0800

    [added] 加入AddScoreAction指令單元測試 [added] 加入GetScoreAction指令單元測試

---
+++ ReportCard.Signalr.Server.Tests/ActionHandler/AddScoreActionHandlerTests.cs
@@ -0,0 +1,42 @@
+﻿
+namespace ReportCard.Signalr.Server.Tests.ActionHandler
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
+    using ReportCard.Domain.Action;
+    using ReportCard.Domain.KeepAliveConn;
+    using ReportCard.Domain.Model;
+    using ReportCard.Domain.Repository;
+    using ReportCard.Signalr.Server.ActionHandler;
+
+    [TestClass]
+    public class AddScoreActionHandlerTests
+    {
+        [TestMethod]
+        public void 新增Score測試()
+        {
+            var repo = new Mock<IScoreRepository>();
+
+            repo.Setup(p => p.Add(1, 80))
+                .Returns((null, new Score()
+                {
+                    f_id = 1,
+                    f_sujectId = 1,
+                    f_point = 80
+                }));
+
+            var handler = new AddScoreActionHandler(repo.Object);
+            var result = handler.ExecuteAction(new ActionModule()
+            {
+                Message = new AddScoreAction()
+                {
+                    SujectId = 1,
+                    Point = 80
+                }.ToString()
+            });
+
+            Assert.IsNull(result.exception);
+            Assert.IsNotNull(result.actionBase);
+        }
+    }
+}
---
+++ ReportCard.Signalr.Server.Tests/ActionHandler/GetScoreActionHandlerTests.cs
@@ -0,0 +1,42 @@
+﻿
+namespace ReportCard.Signalr.Server.Tests.ActionHandler
+{
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
+    using ReportCard.Domain.Action;
+    using ReportCard.Domain.KeepAliveConn;
+    using ReportCard.Domain.Model;
+    using ReportCard.Domain.Repository;
+    using ReportCard.Signalr.Server.ActionHandler;
+
+    [TestClass]
+    public class GetScoreActionHandlerTests
+    {
+        [TestMethod]
+        public void 取Score測試()
+        {
+            var repo = new Mock<IScoreRepository>();
+
+            repo.Setup(p => p.Query(It.IsAny<int?>()))
+                .Returns((null, Enumerable.Range(1, 3).Select(index => new Score()
+                {
+                    f_id = index + 1,
+                    f_sujectId = index + 1,
+                    f_point = (index + 1) * 10
+                })));
+
+            var handler = new GetScoreActionHandler(repo.Object);
+            var result = handler.ExecuteAction(new ActionModule()
+            {
+                Message = new GetScoreSequenceAction()
+                {
+                    SujectId = null
+                }.ToString()
+            });
+
+            Assert.IsNull(result.exception);
+            Assert.IsNotNull(result.actionBase);
+        }
+    }
+}
