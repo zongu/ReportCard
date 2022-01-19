616bd72fa2fe93c82b05ddb4b219f386b4030088
carter <zongu3317@gmail.com>
2022-01-19 11:13:00 +0800

    [added] 加入Signalr Client單元測試專案 [added] 加入ScoreAction指令解析測試 [added] 加入ScoreAddScordProcess測試

---
+++ ReportCard.Signalr.Client.Tests/ActionHandler/ScoreActionHandlerTests.cs
@@ -0,0 +1,36 @@
+﻿
+namespace ReportCard.Signalr.Client.Tests.ActionHandler
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
+    using ReportCard.Domain.Action;
+    using ReportCard.Domain.KeepAliveConn;
+    using ReportCard.Domain.Model;
+    using ReportCard.Signalr.Client.ActionHandler;
+
+    [TestClass]
+    public class ScoreActionHandlerTests
+    {
+        [TestMethod]
+        public void Score通知測試()
+        {
+            var console = new Mock<IConcoleWrapper>();
+
+            var handler = new ScoreActionHandler(console.Object);
+            var result = handler.Execute(new ActionModule()
+            {
+                Message = new ScoreAction()
+                {
+                    Score = new Score()
+                    {
+                        f_id = 1,
+                        f_sujectId = 1,
+                        f_point = 80
+                    }
+                }.ToString()
+            });
+
+            Assert.IsTrue(result);
+        }
+    }
+}
---
+++ ReportCard.Signalr.Client.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ReportCard.Signalr.Client.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ReportCard.Signalr.Client.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2c321661-dfcc-4b66-aec5-05ef10ac01ba")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
